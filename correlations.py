0552cf0eca028cfc4a9e1dbfd3ddba90bf8106f9
Hud-Volodymyr <v.gud2001@gmail.com>
2021-03-08 20:16:10 +0200

    Implement auto and cros correlations

---
+++ correlations.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def expectation(signal):
+    sig_sum = 0
+    for i in range(len(signal)):
+        sig_sum += signal[i]
+    expect = sig_sum / len(signal)
+    return expect
+
+
+def correlate(x, y, t):
+    x_expectation = expectation(x)
+    y_expectation = expectation(y)
+    x_length = len(x)
+    sig_sum = 0
+    for time in range(x_length - t):
+        sig_sum += (x[time] - x_expectation) * (y[time + t] - y_expectation)
+    corr = sig_sum / (x_length - 1)
+    return corr
+
+
+def correlation(x, y, step):
+    correlations = np.zeros(step)
+    for t in range(step):
+        correlations[t] = correlate(x, y, t)
+    return correlations
+
+
+def autocorrelation(x, step):
+    return correlation(x, x, step)
--- random_signals.py
+++ random_signals.py
@@ -1,8 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plotter
-n = 14
-w = 2000
-N = 264
 
 
 def generate_signal(amplitude, phase, frequency, time):
@@ -14,17 +10,7 @@ def signal_generator(harmonics, frequency_max, samples_amount):
     for f in range(1, harmonics + 1):
         phase = np.random.uniform((-np.pi / 2), (np.pi / 2))
         amplitude = np.random.uniform(0, 1)
-        frequency = (f * frequency_max) / harmonics
+        frequency = f * frequency_max / harmonics
         for time in range(samples_amount):
             signal_collection[time] += generate_signal(amplitude, phase, frequency, time)
     return signal_collection
-
-
-signal = signal_generator(n, w, N)
-
-plotter.plot(signal)
-plotter.title('Згенерований сигнал')
-plotter.show()
-
-print('Expected Value is', np.average(signal))
-print('Variance is', np.var(signal))
