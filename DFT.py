8e566af89e1e59f58db98d2fc22b9074a8ab9b4e
Hud-Volodymyr <v.gud2001@gmail.com>
2021-05-10 22:39:17 +0300

    Additional task: table method time compasion

--- DFT.py
+++ DFT.py
@@ -17,3 +17,19 @@ def discrete_fourier_transform(signal):
             sum += xk * wpkN
         spectre[p] = abs(sum)
     return spectre
+
+
+def discrete_fourier_transform_table(signal):
+    N = len(signal)
+    spectre = np.zeros(N)
+    wpk_table = np.zeros((N, N), dtype=complex)
+    for p in range(N):
+        sum = 0
+        for k in range(N):
+            xk = signal[k]
+            wpk_table[p][k] = w(p, k, N)
+            sum += xk * wpk_table[p][k]
+        spectre[p] = abs(sum)
+    #for p in range(N):
+    #    print(p, wpk_table[p])
+    return spectre
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from random_signals import signal_generator
-from DFT import discrete_fourier_transform
+from DFT import discrete_fourier_transform, discrete_fourier_transform_table
 import matplotlib.pyplot as plotter
+from time import time
 
 n = 14
 w = 2000
 N = 264
 
 signal = signal_generator(n, w, N)
-spectre = discrete_fourier_transform(signal)
+
+start1 = time()
+spectre1 = discrete_fourier_transform(signal)
+end1 = time()
+
+start2 = time()
+spectre2 = discrete_fourier_transform_table(signal)
+end2 = time()
+
+print("Час виконання без проміжних значень: ", end1 - start1)
+print("Час виконання з проміжними значеннями: ", end2 - start2)
 
 figure, axis = plotter.subplots(2, 1)
 
@@ -17,7 +28,9 @@ axis[0].plot(range(N), signal)
 axis[0].set_title("Сигнал")
 axis[0].set(xlabel='Час', ylabel='Згенерований сигнал')
 
-axis[1].plot(range(N), spectre)
+axis[1].plot(range(N), spectre1)
 axis[1].set_title("Дискретне перетворення Фур'є")
 axis[1].set(xlabel='p', ylabel='F(p)')
 plotter.show()
+
+
