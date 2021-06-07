6f8d1046c2a45742d5e4c50efb3ce9080befab7e
Hud-Volodymyr <v.gud2001@gmail.com>
2021-06-07 23:21:08 +0300

    Additional task

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random_signals import signal_generator
 from FFT import fft
 import matplotlib.pyplot as plotter
+from DFT import discrete_fourier_transform
 import numpy as np
 
 n = 14
@@ -11,20 +12,22 @@ N = 256
 signal = signal_generator(n, w, N)
 
 spectre = abs(fft(signal))
+dftsig = abs(discrete_fourier_transform(signal))
 
 
-figure, axis = plotter.subplots(2, 1)
+# figure, axis = plotter.subplots(2, 1)
 
-plotter.subplots_adjust(left=0.1, top=0.9, bottom=0.1, right=0.99, hspace=0.5)
+# plotter.subplots_adjust(left=0.1, top=0.9, bottom=0.1, right=0.99, hspace=0.5)
 
-axis[0].plot(range(N), signal)
-axis[0].set_title("Сигнал")
-axis[0].set(xlabel='Час', ylabel='Згенерований сигнал')
+# axis[0].plot(range(N), signal)
+# axis[0].set_title("Сигнал")
+# axis[0].set(xlabel='Час', ylabel='Згенерований сигнал')
 
-axis[1].plot(range(N), spectre)
-axis[1].set_title("Швидке перетворення Фур'є")
-axis[1].set(xlabel='p', ylabel='F(p)')
+# axis[1].plot(range(N), spectre)
+# axis[1].set_title("Швидке перетворення Фур'є")
+# axis[1].set(xlabel='p', ylabel='F(p)')
+# plotter.show()
+
+plotter.stem([a - b for a, b in zip(spectre, dftsig)])
 plotter.show()
-
-
 
